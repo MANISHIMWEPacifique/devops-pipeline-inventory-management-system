1905e4df109fb4a7c0886979a8c8baed7b4be798
MANISHIMWEPacifique <mpacifique29@gmail.com>
2025-11-22 01:13:28 +0200

    feat: add NSG rule for backend API port 5000

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,6 +89,19 @@ resource "azurerm_network_security_group" "main" {
     destination_address_prefix = "*"
   }
 
+  # Allow Backend API Port 5000
+  security_rule {
+    name                       = "Allow-Backend-API"
+    priority                   = 140
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = azurerm_resource_group.main.tags
 }
 
